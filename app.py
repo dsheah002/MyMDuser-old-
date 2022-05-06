31d4f96638a90c7eab5ad9f31ce77a1d8d672246
dsheah002 <85600959+dsheah002@users.noreply.github.com>
2022-05-06 09:52:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -264,7 +264,7 @@ def show_mold():
 
 # Server configurations - REM CHANGE
 # app.run(debug=True, host="api.ap-sg-1.icp.infineon.com", port=6443)
-app.run(debug=True, host='0.0.0.0', port=5000)
+app.run(debug=True, host='0.0.0.0', port=8080)
 
 
 
